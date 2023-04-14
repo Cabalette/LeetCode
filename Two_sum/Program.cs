0c62f09465b964447f80b9e5635973ef0c558390
Dmitriy <123815654+Cabalette@users.noreply.github.com>
2023-04-14 15:16:10 +0300

    go

--- Two_sum/Program.cs
+++ Two_sum/Program.cs
@@ -7,148 +7,90 @@ namespace Two_sum
     {
         static void Main(string[] args)
         {
-            //removeNthFromEnd(new ListNode(3), 4);
+            ListNode listNode1 = new ListNode(23)
+            {
+                next = new ListNode(42)
+                {
+                    next = new ListNode(44)
+                    {
+                        next = new ListNode(44)
+                        {
 
-            MyLinkedList myLinkedList = new MyLinkedList() { val = 1, next = new MyLinkedList() { val = 3 } };
-            myLinkedList.AddAtIndex(1, 2);
-            myLinkedList.AddAtIndex(3, 4);
-            myLinkedList.DeleteAtIndex(6);
+                        }
+                    }
+                }
+            };
+            ListNode listNode2 = new ListNode(1)
+            {
+                next = new ListNode(11)
+                {
+                    next = new ListNode(23)
+                    {
+                        next = new ListNode(42)
+                        {
+
+                        }
+                    }
+                }
+
+            };
+            ListNode res = MergeTwoLists(listNode1, listNode2);
+
+
             int a = 2;
         }
+        public static ListNode? MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null) return list2 == null ? null : list2;
+            else if (list2 == null) return list1;
+
+            ListNode temp;
+            ListNode small = list1.val <= list2.val ? list1 : list2;
+            ListNode newSeq = small;
+            ListNode big = small == list1 ? list2 : list1;
+
+            while (small.next != null && big.next != null)
+            {
+                if (small.next.val <= big.val)
+                {
+                    small = small.next;
+                    continue;
+                }
+                temp = small.next;
+                small.next = big;
+                small = small.next;
+                big = temp;
+            }
+            if (small.next == null) small.next = big;
+            else if (big.next == null)
+            {
+                while (small.next != null)
+                {
+                    if (small.next.val <= big.val)
+                    {
+                        small = small.next;
+                        continue;
+                    }
+                    temp = small.next;
+                    small.next = big;
+                    small = small.next;
+                    big = temp;
+                }
+                small.next = big;
+            }
+            return newSeq;
+        }
+
+
         public class ListNode
         {
             public int val;
-            public ListNode? next;
-            public ListNode(int x)
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
             {
-                val = x;
-                next = null;
+                this.val = val;
+                this.next = next;
             }
-        }
-        public class MyLinkedList
-        {
-            public int val;
-            public MyLinkedList? next;
-            public MyLinkedList()
-            {
-                next = null;
-            }
-            public int GetLength(MyLinkedList pointer, int index)
-            {
-                int len = 1;
-                while (pointer.next != null)
-                {
-                    pointer = pointer.next; len++;
-                }
-                return len;
-            }
-
-            public int Get(int index)
-            {
-                return 0;
-            }
-
-            public void AddAtHead(int val)
-            {
-                MyLinkedList myLinkedList = new MyLinkedList
-                {
-                    val = val,
-                    next = this
-                };
-            }
-
-            public void AddAtTail(int val)
-            {
-                MyLinkedList pointer = this;
-                while (pointer.next != null)
-                {
-                    pointer = pointer.next;
-                }
-                pointer.next = new MyLinkedList() { val = val };
-            }
-
-            public void AddAtIndex(int index, int val)
-            {
-                MyLinkedList pointer = this;
-                if (index == 0)  //first element
-                {
-                    pointer.AddAtHead(val);
-                    return;
-                }
-                int len = pointer.GetLength(pointer, index);
-                if (len == index)  // last element
-                {
-                    this.AddAtTail(val);
-                    return;
-                }
-                else if (index > len) return;
-                //int counter = 1;
-                //while (pointer.next != null)
-                //{
-                //    pointer = pointer.next; counter++;
-                //}
-                //pointer = this;
-                //if (index == counter)
-                //{
-                //    this.AddAtTail(val); return;
-                //}
-                //else if (index > counter) return;
-
-                for (int i = 0; i < index - 1; i++)
-                {
-                    pointer = pointer.next;
-                }
-                pointer.next = new MyLinkedList() //random element
-                {
-                    val = val,
-                    next = pointer.next
-                };
-            }
-
-            public void DeleteAtIndex(int index)
-            {
-                MyLinkedList pointer = this;
-                int len = pointer.GetLength(pointer, index);
-                pointer = this;
-
-                if (index == 0)
-                {
-                    if (len == 1)
-                    {
-                        pointer = null; return;
-                    }
-                    pointer.next = null;
-                }
-
-                if (index > len) return; //wrong index
-                else if (index == len)  //last element
-                {
-                    for (int i = 1; i < len - 1; i++)
-                    {
-                        pointer = pointer.next;
-                    }
-                    pointer.next = null; return;
-                }
-                for (int i = 0; i < index - 1; i++)  //random index
-                {
-                    pointer = pointer.next;
-                }
-                pointer.next = pointer.next.next;
-
-            }
-        }
-        static public ListNode removeNthFromEnd(ListNode head, int n)
-        {
-            ListNode fast = head, slow = head;
-            for (int i = 0; i < n; i++) fast = fast.next;
-            if (fast == null) return head.next;
-            while (fast.next != null)
-            {
-                fast = fast.next;
-                slow = slow.next;
-            }
-            slow.next = slow.next.next;
-            return head;
         }
     }
 }
